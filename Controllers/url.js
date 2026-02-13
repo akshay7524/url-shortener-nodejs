87d46e708a5146b6cfd401480f17e8685d02dcbd
akshay7524 <akshay757524@gmail.com>
2026-02-13 13:38:02 +0530

    Added BASE_URL env variable

--- Controllers/url.js
+++ Controllers/url.js
@@ -4,7 +4,9 @@ import shortid from "shortid";
 export const urlShort = async (req, res) => {
   const longUrl = req.body.longUrl;
   const shortCode = shortid.generate();
-  const shortUrl = `http://localhost:3001/${shortCode}`;
+//  const shortUrl = `http://localhost:3001/${shortCode}`;
+  const shortUrl = `${process.env.BASE_URL}/${shortCode}`;
+
 
   //save to db
   const newUrl = new Url({ shortCode, longUrl });
