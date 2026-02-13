c7801de64766c123743747ea4762aada7c6a398f
akshay7524 <akshay757524@gmail.com>
2026-02-13 12:44:44 +0530

    Deployment ready

--- server.js
+++ server.js
@@ -1,40 +1,30 @@
 
-
 import express from "express";
 import mongoose from "mongoose";
-import { urlShort,getOriginalUrl} from "./Controllers/url.js";
+import dotenv from "dotenv";
+import { urlShort, getOriginalUrl } from "./Controllers/url.js";
 
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true })); // ⭐ IMPORTANT
+app.use(express.urlencoded({ extended: true }));
 
-
-// MongoDB connection
 mongoose
-  .connect(
-    "mongodb+srv://akshay755244:statebank75@zerodhaclonecluster.h43emm9.mongodb.net/?appName=ZerodhaCloneCluster",
-    {
-      dbName: "urlShortenerDB",
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    dbName: "urlShortenerDB",
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-// Test route
-
-  app.get('/',(req,res)=>{
-    res.render("server.ejs",{shortUrl:null})
-  })
-
-    app.post("/shorten", urlShort);
- 
-      app.get("/:shortCode", getOriginalUrl);
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/", (req, res) => {
+  res.render("server.ejs", { shortUrl: null });
 });
 
+app.post("/shorten", urlShort);
+app.get("/:shortCode", getOriginalUrl);
 
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
